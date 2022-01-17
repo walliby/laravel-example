058333546e0e0b423c442b9dab3d58e12fb89602
Joshua Wallace <jwallace@joshuas-air.lan>
2022-01-16 20:00:51 -0500

    add vue scaffolding

---
+++ resources/js/components/AnotherComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>This is another component</div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component 2 mounted.')
+        }
+    }
+</script>
